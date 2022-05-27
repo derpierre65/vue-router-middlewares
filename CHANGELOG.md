a4f813b1c7b3095be9effe2b0e3b2900a89bfa29
derpierre65 <git@derpierre65.de>
2022-05-28 00:40:44 +0200

    fix function components, use the default export as default value

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [1.0.6] - 2022-05-28
+### Fixed
+- If the `default` export for function components is available then use the `default` export as value.
 
 ## [1.0.1] - 2021-05-15
-
 ### Changed
 - Changed the library type to commonjs.
--- src/index.js
+++ src/index.js
@@ -1,144 +1,154 @@
 const routerMiddlewares = {};
 
 function resolveComponents(components) {
-    return Promise.all(components.map(component => {
-        return typeof component === 'function' ? component() : component;
-    }));
+	return Promise.all(components.map((component) => {
+		if (typeof component !== 'function') {
+			return component;
+		}
+
+		return component().then((component) => {
+			return component.default || component;
+		});
+	}));
 }
 
 function getMiddlewares(components) {
-    const middlewares = [];
+	const middlewares = [];
 
-    components
-        .filter(component => component && component.middleware)
-        .forEach(component => {
-            if (Array.isArray(component.middleware)) {
-                middlewares.push(...component.middleware);
-            } else {
-                middlewares.push(component.middleware);
-            }
-        });
+	components
+		.filter((component) => component && component.middleware)
+		.forEach((component) => {
+			if (Array.isArray(component.middleware)) {
+				middlewares.push(...component.middleware);
+			}
+			else {
+				middlewares.push(component.middleware);
+			}
+		});
 
-    return middlewares;
+	return middlewares;
 }
 
 function parseMiddleware(middleware) {
-    if (typeof middleware === 'function') {
-        return {middleware};
-    }
+	if (typeof middleware === 'function') {
+		return { middleware };
+	}
 
-    middleware = middleware.toString(); // force to string
-    let [name, params] = middleware.split(':'); // like auth:xyz
-    if (typeof params === 'string') {
-        params = params.split(',');
-    }
+	middleware = middleware.toString(); // force to string
+	let [name, params] = middleware.split(':'); // like auth:xyz
+	if (typeof params === 'string') {
+		params = params.split(',');
+	}
 
-    return {
-        middleware: name,
-        params
-    };
+	return {
+		middleware: name,
+		params,
+	};
 }
 
 function addMiddleware(name, middleware) {
-    routerMiddlewares[name] = middleware;
+	routerMiddlewares[name] = middleware;
 }
 
 function addMiddlewares(middlewares) {
-    if (typeof middlewares !== 'object' || middlewares === null) {
-        return;
-    }
+	if (typeof middlewares !== 'object' || middlewares === null) {
+		return;
+	}
 
-    for (let key of Object.keys(middlewares)) {
-        routerMiddlewares[key] = middlewares[key];
-    }
+	for (let key of Object.keys(middlewares)) {
+		routerMiddlewares[key] = middlewares[key];
+	}
 }
 
 function addMiddlewaresContext(requireContext) {
-    addMiddlewares(
-        requireContext
-            .keys()
-            .map(file =>
-                [file.replace(/(^.\/)|(\.(js|ts)$)/g, ''), requireContext(file)],
-            )
-            .reduce((guards, [name, guard]) => {
-                return {...guards, [name]: guard.default};
-            }, {})
-    );
+	addMiddlewares(
+		requireContext
+			.keys()
+			.map(file =>
+				[file.replace(/(^.\/)|(\.(js|ts)$)/g, ''), requireContext(file)],
+			)
+			.reduce((guards, [name, guard]) => {
+				return { ...guards, [name]: guard.default };
+			}, {}),
+	);
 }
 
 function getPromise(method) {
-    if (typeof method !== 'function') {
-        return Promise.resolve();
-    }
+	if (typeof method !== 'function') {
+		return Promise.resolve();
+	}
 
-    let promise = method();
-    if (promise instanceof Promise) {
-        return promise;
-    }
-    return Promise.resolve();
+	let promise = method();
+	if (promise instanceof Promise) {
+		return promise;
+	}
+	return Promise.resolve();
 }
 
 const Plugin = {
-    install(Vue, {router, globalMiddlewares = [], beforeLoading, afterLoading}) {
-        function callMiddlewares(middlewares, to, from, next, components) {
-            const _next = (...args) => {
-                // stop if "_next" was called with an argument or the stack is empty
-                if (args.length > 0 || middlewares.length === 0) {
-                    getPromise(afterLoading)
-                        .then(() => {
-                            next(...args);
-                        })
+	install(Vue, { router, globalMiddlewares = [], beforeLoading, afterLoading }) {
+		function callMiddlewares(middlewares, to, from, next, components) {
+			const _next = (...args) => {
+				// stop if "_next" was called with an argument or the stack is empty
+				if (args.length > 0 || middlewares.length === 0) {
+					getPromise(afterLoading)
+						.then(() => {
+							next(...args);
+						});
 
-                    return;
-                }
+					return;
+				}
 
-                const {middleware, params} = parseMiddleware(middlewares.shift());
+				const { middleware, params } = parseMiddleware(middlewares.shift());
 
-                if (typeof middleware === 'function') {
-                    middleware(to, from, _next, params, components);
-                } else if (routerMiddlewares[middleware]) {
-                    routerMiddlewares[middleware](to, from, _next, params, components);
-                } else {
-                    throw Error('Undefined middleware ' + middleware);
-                }
-            };
+				if (typeof middleware === 'function') {
+					middleware(to, from, _next, params, components);
+				}
+				else if (routerMiddlewares[middleware]) {
+					routerMiddlewares[middleware](to, from, _next, params, components);
+				}
+				else {
+					throw Error('Undefined middleware ' + middleware);
+				}
+			};
 
-            _next();
-        }
+			_next();
+		}
 
-        router.beforeEach(async (to, from, next) => {
-            let components = [];
+		router.beforeEach(async (to, from, next) => {
+			let components = [];
 
-            try {
-                // get matched components and resolve them
-                components = await resolveComponents(router.getMatchedComponents(to));
-            } catch (error) {
-                if (/^Loading( CSS)? chunk (\d)+ failed\./.test(error.message)) {
-                    window.location.reload(true);
-                    return;
-                }
-            }
+			try {
+				// get matched components and resolve them
+				components = await resolveComponents(router.getMatchedComponents(to));
+			}
+			catch (error) {
+				if (/^Loading( CSS)? chunk (\d)+ failed\./.test(error.message)) {
+					window.location.reload(true);
+					return;
+				}
+			}
 
-            // no components found
-            if (components.length === 0) {
-                return next();
-            }
+			// no components found
+			if (components.length === 0) {
+				return next();
+			}
 
-            getPromise(beforeLoading)
-                .then(() => {
-                    callMiddlewares([
-                        ...globalMiddlewares,
-                        ...getMiddlewares(components)
-                    ], to, from, next, components);
-                });
-        });
-    }
-}
+			getPromise(beforeLoading)
+				.then(() => {
+					callMiddlewares([
+						...globalMiddlewares,
+						...getMiddlewares(components),
+					], to, from, next, components);
+				});
+		});
+	},
+};
 
 export {
-    Plugin as default,
-    addMiddleware,
-    addMiddlewares,
-    addMiddlewaresContext,
-    routerMiddlewares,
-}
+	Plugin as default,
+	addMiddleware,
+	addMiddlewares,
+	addMiddlewaresContext,
+	routerMiddlewares,
+};
